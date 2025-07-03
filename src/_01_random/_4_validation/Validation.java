866bcd5585ecb856d181e4ac8d8091c04a10c250
nolanbooth <nb@nb.org>
2025-07-02 19:57:36 -0700

    IDK

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -9,17 +9,31 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
-		Random randomMaker = new Random();
-		
-		int randomNumber = randomMaker.nextInt(5);
-		
-		System.out.println(randomNumber);
 
-		// 1. Use each value of randomNumber to give the user a random compliment.
-		if()
+		Random randomMaker = new Random();
+
+		int randomNumber = randomMaker.nextInt(4);
+
+		for(int iForgor=0;iForgor<10;iForgor+=1 ) {
+			
+			
+			
+			// 1. Use each value of randomNumber to give the user a random compliment.
+			if(randomNumber==0) {
+				System.out.println("Your Amazing!");
+			}
+			if(randomNumber==1) {
+				System.out.println("Your SO Cool!");
+			}
+			if(randomNumber==2) {
+				System.out.println("Your Ugly");
+			}
+			if(randomNumber==3) {
+				System.out.println("Your IDK");
+			}
+		}
 		// 2. Repeat all the code above 10 times
-		
+
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -4,34 +4,50 @@
 
 package _01_random._5_magic_8_ball;
 
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
+	
+public static void main(String[] args) {
+	
 
 	// 1. Make a main method that includes all the steps below
-
-	// 2. Get the user to enter a question for the 8 ball to answer
 	
+	// 2. Get the user to enter a question for the 8 ball to answer
+	String idk = JOptionPane.showInputDialog("Ask Away");
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
 	//     Limit the random numbers to be between 0 and 3
-
-	
+	Random cheesey = new Random(new Date().getTime());
+	int quesadilla = cheesey.nextInt(4);
 	// 4. If the random number is 0
-
+	if(quesadilla==0) {
+		JOptionPane.showMessageDialog(null, "Yes yes sir of course 100%");
+	}
 	// -- tell the user "Yes"
 
 	// 5. If the random number is 1
-
+	if(quesadilla==1) {
+		JOptionPane.showMessageDialog(null, "NO NO NO! HELL NO! HOW COULD YOU THINK THAT!");
+	}
 	// -- tell the user "No"
 
 	// 6. If the random number is 2
-
+	if(quesadilla==2) {
+		JOptionPane.showMessageDialog(null, "That wisdome will come to you when you desire it most...");
+	}
 	// -- tell the user "Maybe you should ask Google?"
 
 	// 7. If the random number is 3
-
+	if(quesadilla==3) {
+		JOptionPane.showMessageDialog(null, "I don't wanna tell u");
+	}
 	// -- write your own answer
 
+
+}
 }
